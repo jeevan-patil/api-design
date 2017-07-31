eb679d870fb67d52e21d0ecd63bd515034d6f004
Jeevan Patil <jeevanpaatil@gmail.com>
2017-07-31 16:07:13 +0530

    hide custom classes from swagger documentation

--- src/main/java/xyz/jeevan/api/config/SwaggerConfig.java
+++ src/main/java/xyz/jeevan/api/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package xyz.jeevan.api.config;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,10 +14,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import xyz.jeevan.api.key.ProjectKey;
 
 /**
- * Created by jeevan on 16/7/17.
- * Swagger2 configurations.
+ * Created by jeevan on 16/7/17. Swagger2 configurations.
  */
 @Configuration
 @EnableSwagger2
@@ -23,7 +25,8 @@ public class SwaggerConfig {
 
   @Bean
   public Docket projectApi() {
-    return new Docket(DocumentationType.SWAGGER_2).groupName("rest-api").apiInfo(apiInfo()).select()
+    return new Docket(DocumentationType.SWAGGER_2).ignoredParameterTypes(ignoreParamClasses())
+        .groupName("rest-api").apiInfo(apiInfo()).select()
         .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
         .build();
   }
@@ -39,5 +42,14 @@ public class SwaggerConfig {
         .contact(new Contact("Jeevan Patil",
             "https://github.com/jeevan-patil",
             "jeevanpaatil@gmail.com")).build();
+  }
+
+  /**
+   * Hide classes which are used as Keys for security checks.
+   */
+  private Class[] ignoreParamClasses() {
+    Set<Class<? extends Object>> ignoredClasses = new HashSet<>();
+    ignoredClasses.add(ProjectKey.class);
+    return ignoredClasses.toArray(new Class[ignoredClasses.size()]);
   }
 }
