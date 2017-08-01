d3540ed44c93ded08236793630bca45580d7273d
Jeevan Patil <jeevanpaatil@gmail.com>
2017-08-01 16:03:18 +0530

    project assumption value is string, so no need to check for numeric operations

--- src/main/java/xyz/jeevan/api/predicate/ProjectAssumptionPredicate.java
+++ src/main/java/xyz/jeevan/api/predicate/ProjectAssumptionPredicate.java
@@ -1,11 +1,9 @@
 package xyz.jeevan.api.predicate;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.core.types.dsl.StringPath;
 import xyz.jeevan.api.domain.ProjectAssumption;
-import xyz.jeevan.api.helper.SearchCriteriaBuilder;
 import xyz.jeevan.api.key.SearchCriteria;
 
 public final class ProjectAssumptionPredicate {
@@ -14,30 +12,23 @@ public final class ProjectAssumptionPredicate {
     PathBuilder<ProjectAssumption> entityPath = new PathBuilder<>(ProjectAssumption.class,
         "projectassumption");
 
-    String value1 = String.valueOf(searchCriteria.getValue1());
-    if (SearchCriteriaBuilder.isNumeric(searchCriteria.getOperation())) {
-      NumberPath<Integer> path = entityPath.getNumber(searchCriteria.getKey(), Integer.class);
-      int intVal1 = Integer.parseInt(value1);
-      switch (searchCriteria.getOperation()) {
-        case "<=":
-          return path.loe(intVal1);
-        case ">":
-          return path.gt(intVal1);
-        case "<":
-          return path.lt(intVal1);
-        case ">=":
-          return path.goe(intVal1);
-      }
-    } else {
-      StringPath path = entityPath.getString(searchCriteria.getKey());
-      switch (searchCriteria.getOperation()) {
-        case ":":
-          return path.containsIgnoreCase(value1);
-        case "=":
-          return path.equalsIgnoreCase(value1);
-        case "!=":
-          return path.notEqualsIgnoreCase(value1);
-      }
+    String value = String.valueOf(searchCriteria.getValue1());
+    StringPath path = entityPath.getString(searchCriteria.getKey());
+    switch (searchCriteria.getOperation()) {
+      case "<=":
+        return path.loe(value);
+      case ">":
+        return path.gt(value);
+      case "<":
+        return path.lt(value);
+      case ">=":
+        return path.goe(value);
+      case ":":
+        return path.containsIgnoreCase(value);
+      case "=":
+        return path.equalsIgnoreCase(value);
+      case "!=":
+        return path.notEqualsIgnoreCase(value);
     }
     return null;
   }
